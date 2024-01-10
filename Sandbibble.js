eb0d0164c592bbf893feefcae76fd231666a2525
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2024-01-10 08:15:56 -0500

    Initial Commit

---
+++ Sandbibble.js
@@ -0,0 +1,13 @@
+//Programmer: Madelyn Hershberger
+//Date: 1.10.2024
+//Program: Sphero Bolt Testing
+
+async function startProgram() {
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
+}
+
