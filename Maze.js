206a7bd77302e66bd9791943abe9b5106b6f8f00
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2024-01-22 08:41:12 -0500

    Speak Start code - SpheroBolt

--- Maze.js
+++ Maze.js
@@ -4,3 +4,8 @@
 // Program: Maze
 // Bolt ID: 52F9
 
+//Start message
+async function startProgram() {
+await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
+await speak("Start", true);
+}
