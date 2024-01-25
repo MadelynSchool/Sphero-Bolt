e8a1cfc3dedb28d8a978af8ff299a0d1efa88dd2
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2024-01-25 08:14:37 -0500

    Blue Checkpoint
    Moves forward, turns blue at checkpoint

--- Maze.js
+++ Maze.js
@@ -4,8 +4,16 @@
 // Program: Maze
 // Bolt ID: 52F9
 
-//Start message
+/*
+// Start Maze
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
+*/
+
+//Blue: Checkpoint 1
+async function startProgram() {
+	await roll(0, 50, 3)
+	await delay (1)
+	setMainLed({ r: 0, g: 0, b: 255 });
 }
