523ff63d72f934e5ec661c44d5403114d4d3f83e
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2024-01-29 08:28:56 -0500

    Checkpoint 2
    Plays animal sound at checkpoint after moving forward

--- Maze.js
+++ Maze.js
@@ -9,11 +9,15 @@
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
-*/
+
 
 //Blue: Checkpoint 1
-async function startProgram() {
 	await roll(0, 50, 3)
 	await delay (1)
 	setMainLed({ r: 0, g: 0, b: 255 });
-}
+	*/
+//Checkpoint 2: Sound 1
+async function startProgram() {
+	await roll (90, 50, 2)
+	await delay (1)
+	await Sound.Animal.play(true)
