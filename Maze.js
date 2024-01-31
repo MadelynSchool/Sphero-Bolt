af953f586364fb01cd81de4231311ae26cc18bea
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2024-01-31 07:45:33 -0500

    Checkpoint 4
    Plays sound

--- Maze.js
+++ Maze.js
@@ -9,22 +9,27 @@
 async function startProgram() {
 await scrollMatrixText('Start', { r: 66, g: 56, b: 255 }, 30, true)
 await speak("Start", true);
-
+*/
 
 //Blue: Checkpoint 1
+async function startProgram() {
 	await roll(0, 50, 3)
 	await delay (1)
 	setMainLed({ r: 0, g: 0, b: 255 });
-	*/
-//Checkpoint 2: Sound 1
-async function startProgram() {
-	await roll (90, 50, 2)
-	await delay (1)
-	await Sound.Animal.play(true)
 
+//Checkpoint 2: Sound 1
+	await roll (90, 50, 2.2)
+	await delay (1)	
+	await Sound.Animal.play(true)
 //Checkpoint 3: Red
 	await roll (180, 50, 1.5)
 	await delay (1)
 	await roll (220, 65, 1)
 	await delay (1)
 	setMainLed({ r: 255, g: 0, b: 0 });
+
+//Checkpoint 4: Sound #2
+	await roll (120, 50, 0.8)
+	await delay (1)
+	await roll (45, 55, 1.1)
+	await Sound.Animal.play(true)
